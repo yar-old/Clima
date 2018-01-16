aedfe3a04ed59d63705ee5df1069f07a9e300644
Yuri Ramocan <yramocan@umich.edu>
2018-01-16 14:17:55 -0500

    Update WeatherManager for Networking handling

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -9,28 +9,41 @@
 import Foundation
 import Alamofire
 import SwiftyJSON
-import os
 
 class WeatherManager {
-    static func fetchWeatherData(withURL url: String, AppID appID: String, andParams params: [String:String], callback: @escaping (_ data: JSON, _ error: Error?) -> Void) {
+    static func fetchWeatherData(withURL url: String, AppID appID: String, andParams params: [String:String], completion: @escaping (_ data: JSON, _ error: Error?) -> Void) {
         Alamofire.request(url, method: .get, parameters: params)
             .validate(contentType: ["application/json"])
             .responseJSON { (response) in
                 guard response.result.isSuccess else {
                     print("\(response.result.error.debugDescription)")
-                    callback(JSON.null, WeatherDataError.badServerResponse)
+                    completion(JSON.null, WeatherDataError.badServerResponse)
                     return
                 }
                 
                 guard let json = response.result.value else {
                     print("JSON ERROR: \(response.result.error.debugDescription)")
-                    callback(JSON.null, WeatherDataError.invalidJSON)
+                    completion(JSON.null, WeatherDataError.invalidJSON)
                     return
                 }
                 
-                callback(JSON(json), nil)
+                completion(JSON(json), nil)
         }
     }
+    
+    static func updateWeatherData(json: JSON, weatherModel: WeatherDataModel) throws {
+        guard let temperature = json["main"]["temp"].double,
+              let condition = json["weather"][0]["id"].int,
+              let city = json["name"].string else {
+            throw WeatherDataError.invalidJSON
+        }
+        
+        weatherModel.temperature = temperature
+        weatherModel.condition = condition
+        weatherModel.city = city
+        weatherModel.weatherIconName = weatherModel.updateWeatherIcon(condition: condition)
+    }
+    
 }
 
 enum WeatherDataError: Error {
