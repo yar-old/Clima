40332ddd97489a6e9f1bbc4a9caa0405b75c715d
Yuri Ramocan <yramocan@umich.edu>
2018-01-16 14:19:10 -0500

    Create Weather Data Model to handle weather data

--- Clima/WeatherDataModel.swift
+++ Clima/WeatherDataModel.swift
@@ -11,50 +11,46 @@ import UIKit
 class WeatherDataModel {
 
     //Declare your model variables here
+    var temperature: Double = 0
+    var condition: Int = 0
+    var city: String = ""
+    var weatherIconName: String = ""
     
+    func tempF() -> Int {
+         return Int((temperature * (9/5) - 459.67).rounded())
+    }
     
-    //This method turns a condition code into the name of the weather condition image
+    func tempC() -> Int {
+        return Int((temperature - 273.15).rounded())
+    }
     
-//    func updateWeatherIcon(condition: Int) -> String {
-//        
-//    switch (condition) {
-//    
-//        case 0...300 :
-//            return "tstorm1"
-//        
-//        case 301...500 :
-//            return "light_rain"
-//        
-//        case 501...600 :
-//            return "shower3"
-//        
-//        case 601...700 :
-//            return "snow4"
-//        
-//        case 701...771 :
-//            return "fog"
-//        
-//        case 772...799 :
-//            return "tstorm3"
-//        
-//        case 800 :
-//            return "sunny"
-//        
-//        case 801...804 :
-//            return "cloudy2"
-//        
-//        case 900...903, 905...1000  :
-//            return "tstorm3"
-//        
-//        case 903 :
-//            return "snow5"
-//        
-//        case 904 :
-//            return "sunny"
-//        
-//        default :
-//            return "dunno"
-//        }
-//
-//    }
+    func updateWeatherIcon(condition: Int) -> String {
+        
+        switch (condition) {
+            case 0...300 :
+                return "tstorm1"
+            case 301...500 :
+                return "light_rain"
+            case 501...600 :
+                return "shower3"
+            case 601...700 :
+                return "snow4"
+            case 701...771 :
+                return "fog"
+            case 772...799 :
+                return "tstorm3"
+            case 800 :
+                return "sunny"
+            case 801...804 :
+                return "cloudy2"
+            case 900...903, 905...1000  :
+                return "tstorm3"
+            case 903 :
+                return "snow5"
+            case 904 :
+                return "sunny"
+            default :
+                return "dunno"
+            }
+    }
 }
