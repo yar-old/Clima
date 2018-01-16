e6743bfe421d13b8e2e65b1788f9e7dcddbec719
Yuri Ramocan <yramocan@umich.edu>
2018-01-16 14:17:33 -0500

    Update WeatherVC

---
+++ Clima/WeatherViewController+CLLocationManagerDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherViewController+CLLocationManagerDelegate.swift
+//  Clima
+//
+//  Created by Yuri Ramocan on 1/13/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
